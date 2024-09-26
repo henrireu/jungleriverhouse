0badc522527a578db271ee836f1be1ffde98cbce
reijomo <95440863+reijomo@users.noreply.github.com>
2024-09-26 11:41:16 +0300

    footer

---
+++ src/components/contactFooter.tsx
@@ -0,0 +1,37 @@
+import tripadvisor from '../images/trip-advisor.png';
+
+const ContactFooter = () => {
+  return (
+    <div className="footer-container">
+      <div className="contact-container">
+        <p>Jungle River House © 2024 All Rights Reserved.</p>
+        <p>Jl Orangutan, Bukit Lawang, North Sumatra, ID</p>
+        <p>081375278600</p>
+      </div>
+      <a href="https://www.tripadvisor.com/Hotel_Review-g680012-d23295489-Reviews-Jungle_River_House-Bukit_Lawang_North_Sumatra_Sumatra.html" target="_blank" rel="noopener noreferrer">
+        <img className="tripadvisor-logo" src={tripadvisor} alt="tripadvisor logo" />
+      </a>
+      <div className="follow-us-container">
+        <div className="follow-us-header">
+          <p>FOLLOW US <br/> (täytyy hankkia logot)</p>
+        </div>
+        <div className="logo-container">
+          <div className="logo-circle">
+            IG
+          </div>
+          <div className="logo-circle">
+            BO
+          </div>
+          <div className="logo-circle">
+            GO
+          </div>
+          <div className="logo-circle">
+            TR
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ContactFooter;
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -2,19 +2,15 @@ import blackGibbonRoom from '../../images/rooms/black-gibbon-room.jpg';
 import familyRoom2 from '../../images/rooms/familyroom2.jpg';
 import orangutanRoom from '../../images/rooms/orangutan-room.jpg';
 
-//import Card from 'react-bootstrap/Card';
-//import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-//import Image from 'react-bootstrap/Image';
- 
-//import bukitImage from '../../images/jungle-river-house-bukit.jpg';
 
 import HomeRoom from '../home/homeRoom';
 import RoomCarousel from '../roomCarousel';
 import JungleTrekSection from './jungleTrekSection';
 import ReviewSection from './reviewSection';
+import ContactFooter from '../contactFooter';
 
 interface WindowWidthProps {
   windowWidth: number;
@@ -22,27 +18,14 @@ interface WindowWidthProps {
 
 const Home:React.FC<WindowWidthProps> = ({ windowWidth }) => {
   return (
-    <div className="bg-color1" style={{ /*backgroundColor: '#004D40' '#00695C',*/ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-      {/*<div style={{ display: 'flex', gap: 50, padding: 50}}>
-        {windowWidth > 900 && (
-          <Image 
-            src={bukitImage} 
-            rounded 
-            style={{ width: '50%', height: windowWidth > 1200 ? 700 : 500}}
-          />
-        )
-        {/*<IntroductionTextCard />*//*}
-      </div>*/}
+    <div className="bg-color1" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
       <JungleTrekSection />
       <RoomsSection windowWidth={windowWidth}/>
       <ReviewSection />
+      <ContactFooter />
     </div>
   )
 }
-
-// tee tuo kuntoon että kaikki huoneet on keskellä vaikka ei oo vierekkäin
-
-
 
 const RoomsSection:React.FC<WindowWidthProps> = ({ windowWidth }) => {
   return (
@@ -68,29 +51,5 @@ const RoomsSection:React.FC<WindowWidthProps> = ({ windowWidth }) => {
     </div>
   )
 }
-
-/*const IntroductionTextCard = () => {
-  return (
-    <Card className="text-center" style={{ backgroundColor: 'transparent', border: 'none', color: 'white'}}>
-      <Card.Body>
-        <h1 className="jungle-header" style={{ marginBottom: 10}}>MOI!</h1>
-        <Card.Title className="fs-3" style={{ marginBottom: 10}}>TERVETULOA!</Card.Title>
-        <Card.Text className="fs-4">
-        Haluatko kokea sademetsän ja oman viidakkoseikkailusi luonnossa villieläimiä kanssa?
-        </Card.Text>
-        <Card.Text className="fs-4">
-        Olemme sinun palveluksessasi.
-        </Card.Text>
-        <Card.Text className="fs-4">
-        Järjestämme toivomasi viidakkoretket halusitpa kokea päiväretken tai syventää kokemustasi yön yli kestävillä retkillä. Viidakkoretkemme tarjoavat sinulle uskomattoman kokemuksen yhteen maailman hienoimmista jäljellä olevista sademetsistä. Tervetuloa Gunung Lesuerin kansallispuistoon, Sumatralle.
-        </Card.Text>
-        <Card.Text className="fs-4">
-        Majoituspaikkamme tarjoaa sinulle viihtyisästi sisustetut huoneet monilla mukavuuksilla ja näköalan joelle. Hotellin yhteydessä on myös ravintola aivan joen rannalla, jossa voit nauttia hyvästä indonesialaisesta tai länsimaalaisesta ruoasta, juomista sekä viidakon tunnelmasta. Sijaintimme on ihan keskeisellä paikalla Bukit Lawangissa.
-        </Card.Text>
-        <Button variant="outline-light" size="lg">More about us</Button>
-      </Card.Body>
-    </Card>
-  );
-}*/
-
+    
 export default Home;
